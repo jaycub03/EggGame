25e34f11a323fbc6979b348c61dda5b069c2587b
Vinh Ta <tavinh132@gmail.com>
2024-04-10 18:38:57 -0700

    added a player list

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var express = require('express');   // express module, web framework
 var app = express();                // instance of express called app  
 var server = require('http').Server(app);       // supplied the app to the HTTP server
 
-var io = require('socket.io')(server);   // listen to server
+var io = require('socket.io')(server);  // listen to server
+var players = {};                       // players object
+
 
 app.use(express.static(__dirname));  // render static files using express.static
 
@@ -14,10 +16,23 @@ app.get('/', function (req, res) {              // tells the server to serve the
 });
 
 // connections and disconnections
-io.on('connection', function (socket) {
+io.on('connection', function (socket) {                     // player joining
     console.log ('a caretaker has entered the room');
-    socket.on('disconnect', function() {
-        console.log ('a caretaker has left the room');
+    // create a new player and add it to the players object
+    players[socket.id] = {
+        playerID: socket.id,
+        cursorLocation: 0,
+        mouseDown: false
+    };
+    console.log(players);
+    socket.emit('currentPlayers',players);                  // send the players object to the new player
+    socket.broadcast.emit('newPlayer',players[socket.id]);  // update all other players of the new player
+
+    socket.on('disconnect', function() {                    // player disconnecting
+        // console.log ('a caretaker has left the room');   // debug check player list
+        // remove the player from the players object
+        delete players[socket.id];
+        socket.disconnect(socket.id);
     });
 });
 
